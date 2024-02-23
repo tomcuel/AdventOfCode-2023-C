82870a4908e16314480c647766469c123daba1b5
Tom Cuel <tom.cuel3@gmail.com>
2024-02-23 23:59:33 +0100

    update

---
+++ day6/day_06_p2.py
@@ -0,0 +1,19 @@
+with open("input6.txt") as fin:
+    lines = fin.read().strip().split("\n")
+
+
+t = int("".join(lines[0].split()[1:]))
+d = int("".join(lines[1].split()[1:]))
+
+
+def ways(t, d):
+    count = 0
+    for i in range(t):
+        # Hold down for i seconds
+        if (t - i) * i > d:
+            count += 1
+
+    return count
+
+
+print(ways(t, d))
